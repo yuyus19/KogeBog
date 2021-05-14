ddc11e664d98f58c118fd718020c3f2b8bffe635
yuyus19 <56111176+yuyus19@users.noreply.github.com>
2021-05-14 15:56:07 +0200

    Included maven onto build.gradle

--- app/src/main/java/com/example/kogebog/favouriteOption/FavouritesFragment.kt
+++ app/src/main/java/com/example/kogebog/favouriteOption/FavouritesFragment.kt
@@ -16,7 +16,6 @@ class FavouritesFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
     }
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
