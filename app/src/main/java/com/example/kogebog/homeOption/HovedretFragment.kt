ebbf2b73f780919359f6ff8a9529103225cc0825
mShadow_56 <almighty2300@gmail.com>
2021-05-15 17:05:25 +0200

    med to opskrifter

--- app/src/main/java/com/example/kogebog/homeOption/HovedretFragment.kt
+++ app/src/main/java/com/example/kogebog/homeOption/HovedretFragment.kt
@@ -6,55 +6,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dicerally.R
+import com.example.dicerally.databinding.FragmentHovedretBinding
+import com.example.dicerally.databinding.FragmentRecipeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HovedretFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HovedretFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+
+    private var _binding: FragmentHovedretBinding?=null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hovedret, container, false)
+        _binding = FragmentHovedretBinding.inflate(inflater, container, false)
+
+
+        binding.button1.setOnClickListener {
+         val array = resources.getStringArray(R.array.Lasagne)
+            binding.titelOpskrift.setText(array[0])
+            binding.ingredientsOpskrift.setText(array[1])
+            binding.manual.setText(array[2])
+
+
+
+        }
+        binding.button2.setOnClickListener {
+            val array = resources.getStringArray(R.array.Spaghetti)
+            binding.titelOpskrift.setText(array[0])
+            binding.ingredientsOpskrift.setText(array[1])
+            binding.manual.setText(array[2])
+        }
+
+
+
+
+
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HovedretFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HovedretFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
 }
