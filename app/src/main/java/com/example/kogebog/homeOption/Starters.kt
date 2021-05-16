7bd92320f860124c3bdb3c7cf16197d34cbcab55
Kenneth <almighty2300@gmail.com>
2021-05-16 17:40:44 +0200

    starten på forretter er blevet indsat

---
+++ app/src/main/java/com/example/kogebog/homeOption/Starters.kt
@@ -0,0 +1,24 @@
+package com.example.kogebog.homeOption
+
+object Starters {
+    var Headline: Array<String?>? = arrayOf(
+        "Røget laks med agurkecreme",
+        "Bruchetta med tomat og basilikum",
+        "Kartorffel suppe",
+        "Tun salat",
+        "Tarteletter"
+
+    )
+    var Ingridients: Array<String?>?= arrayOf(
+        "300 g røget laks\n0,5 argurk\n3 forårsløg\n200 g fromage frais\n 1 bdt. basilikum\nfine salatblade\n100 g lakserogn\n12 grønne asparges fra glas\n4 skiver toastbrød\nsalt og peber",
+        "6-8 skiver lækkert brød - fx surdejsbrød eller grydebrød\n1 spsk olivenolie\n1 fed hvidløg\n10 tomat, solmodne, skåret i tern\nMarinade til tomater\n1 knivspids honning\n1 spsk olivenolie\n1 fed hvidløg, knust\nfrisk basilikum, 10 blade, helt fintstrimlet\n0,50 tsk flagesalt\nsort peber, friskkværnet\nDerudover\n70 g parmaskinke, eller serrano (kan undlades)\nfrisk basilikum, 20 blade, skåret i strimler",
+    )
+    var Procedure: Array<String?>? = arrayOf(
+        "Skræl agurken, flæk den og skrab kernerne ud med en teske. Skær frugtkødet i tern, drys med lidt salt og lad dem trække i ca. 1⁄2 t. Skyl dem, lad dem dryppe af og dup dem tørre med køkkenrulle.\nSkær forårsløgene i smalle ringe og brug mest muligt af den grønne del.\nSmag fromage frais til med salt og peber og tilsæt masser af snittet, frisk basilikum. Vend agurketern og forårsløg i.\nLæg fine salatblade på 4 tallerkener og fordel skiver af røget laks dekorativt på dem. Læg agurkecremen ved siden af og anbring en skefuld lakserogn ovenpå den.\nPynt anretningen med grønne asparges og læg 2 trekanter ristet brød på tallerkenkanten.\n",
+        "Rør marinaden sammen og vend tomaterne deri. Lad dem stå i skålen, mens brødene ristes.\nRist brødskiverne på brødrister eller i ovnen, og gnid de ristede brød med et fed hvidløg, hvorefter de pensles med en smule olivenolie.\nHæld overskydende marinade og saft fra tomaterne og top de ristede brød med tomater.\nFordel derefter parmaskinke eller serranoskinke over tomaterne – eller undlad dette hvis vegetar – og drys med fintstrimlet basilikum.",
+    )
+
+
+
+
+}
