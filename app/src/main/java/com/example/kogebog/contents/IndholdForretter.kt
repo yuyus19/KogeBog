1e59f7fb33ef503def2ad8e4c88e8baa9a8f9479
yuyus19 <56111176+yuyus19@users.noreply.github.com>
2021-05-18 12:42:03 +0200

    Changes to arrayOf() in contents.

--- app/src/main/java/com/example/kogebog/contents/IndholdForretter.kt
+++ app/src/main/java/com/example/kogebog/contents/IndholdForretter.kt
@@ -1,21 +1,21 @@
 package com.example.kogebog.contents
 
 object IndholdForretter {
-    var title: Array<String?>? = arrayOf(
+    var title = arrayOf(
         "Røget laks med agurkecreme",
         "Bruchetta med tomat og basilikum",
         "Kartorffel suppe",
         "Tun salat",
         "Tarteletter"
     )
-    var ingredients: Array<String?>?= arrayOf(
+    var ingredients = arrayOf(
         "300 g røget laks\n0,5 argurk\n3 forårsløg\n200 g fromage frais\n 1 bdt. basilikum\nfine salatblade\n100 g lakserogn\n12 grønne asparges fra glas\n4 skiver toastbrød\nsalt og peber",
         "6-8 skiver lækkert brød - fx surdejsbrød eller grydebrød\n1 spsk olivenolie\n1 fed hvidløg\n10 tomat, solmodne, skåret i tern\nMarinade til tomater\n1 knivspids honning\n1 spsk olivenolie\n1 fed hvidløg, knust\nfrisk basilikum, 10 blade, helt fintstrimlet\n0,50 tsk flagesalt\nsort peber, friskkværnet\nDerudover\n70 g parmaskinke, eller serrano (kan undlades)\nfrisk basilikum, 20 blade, skåret i strimler",
         "10 g smør\nca. 75 g grofthakket zittaurløg\n500 g kartofler i tern\n350 g porrer i ringe-den hvide del\n3/4Grøntsagsbouillon\n1 dl piskefløde\n1 tsk groft salt\nFriskkværnet hvid peber",
         "2 dåser tun i vand\n4 spsk mayonaise\n50 g majskerner\nflagesalt\nsort peber, friskkværnet\n3 spsk purløg, finthakket",
         "2 hønsebryst\n3 dl grøntsagsbouillon\n1 gulerod, i tern\n1 løg, i tern\n2 laurbærblade\n5 peberkorn\n1 tsk salt40 g smør\n2 spsk hvedemel\n4 dl mælk\nsalt\nsort peber, friskkværnet\n200 g hvide asparges12 tarteletter\n1 håndfuld persille\nsort peber, friskkværnet\nflagesalt"
         )
-    var procedure: Array<String?>? = arrayOf(
+    var procedure = arrayOf(
         "Skræl agurken, flæk den og skrab kernerne ud med en teske. Skær frugtkødet i tern, drys med lidt salt og lad dem trække i ca. 1⁄2 t. Skyl dem, lad dem dryppe af og dup dem tørre med køkkenrulle.\nSkær forårsløgene i smalle ringe og brug mest muligt af den grønne del.\nSmag fromage frais til med salt og peber og tilsæt masser af snittet, frisk basilikum. Vend agurketern og forårsløg i.\nLæg fine salatblade på 4 tallerkener og fordel skiver af røget laks dekorativt på dem. Læg agurkecremen ved siden af og anbring en skefuld lakserogn ovenpå den.\nPynt anretningen med grønne asparges og læg 2 trekanter ristet brød på tallerkenkanten.\n",
         "Rør marinaden sammen og vend tomaterne deri. Lad dem stå i skålen, mens brødene ristes.\nRist brødskiverne på brødrister eller i ovnen, og gnid de ristede brød med et fed hvidløg, hvorefter de pensles med en smule olivenolie.\nHæld overskydende marinade og saft fra tomaterne og top de ristede brød med tomater.\nFordel derefter parmaskinke eller serranoskinke over tomaterne – eller undlad dette hvis vegetar – og drys med fintstrimlet basilikum.",
         "Lad smørret smelte ved kraftig varme, men uden at det bruner. Svits løg og kartofler i ca. 2 min. Tilsæt porrer og svits i yderligere ca. 1 min.\nHæld bouillon ved og lad suppen koge i ca. 15 min. - eller til grøntsagerne er møre.\nBlend suppen til den er jævn - enten med en stavblender eller i en almindelig blender.\nKom suppen tilbage i gryden sammen med fløde, salt og peber. Bring suppen i kog og smag den til.",
